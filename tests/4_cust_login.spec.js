89992b15d13aab2632cf621749b4b56a2346e0bf
lucky <luckykumaryadav781@gmail.com>
2025-01-07 22:53:31 +0530

    Stop tracking .env file

--- tests/4_cust_login.spec.js
+++ tests/4_cust_login.spec.js
@@ -1,10 +1,17 @@
 import { test, expect } from '@playwright/test';
+require('dotenv').config();
 
 test('test', async ({ page }) => {
-  await page.goto('https://parabank.parasoft.com/parabank/index.htm');
-  await page.locator('input[name="username"]').click();
-  await page.locator('input[name="username"]').fill('test');
-  await page.locator('input[name="password"]').click();
-  await page.locator('input[name="password"]').fill('Admin123');
-  await page.getByRole('button', { name: 'Log In' }).click();
+    // Destructure environment variables
+    const { BASE_URL, USER_NAME, PASSWORD } = process.env;
+
+    // Navigate to the URL
+    await page.goto(BASE_URL);
+
+    // Fill in the username and password fields
+    await page.locator('input[name="username"]').fill(USER_NAME);
+    await page.locator('input[name="password"]').fill(PASSWORD);
+
+    // Click the login button
+    await page.locator('input[type="submit"][value="Log In"]').click();  // Updated to use a more common selector for login buttons
 });
